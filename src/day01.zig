b6a925306a857cd4b1b59a1c40a024aa2bba73ee
mathroy0310 <mathroy0310@gmail.com>
2023-12-01 12:58:46 -0500

    day01-fixed-skill-issue

--- src/day01.zig
+++ src/day01.zig
@@ -1,18 +1,21 @@
+const std = @import("std");
 const util = @import("utils.zig");
+const fmt = std.fmt;
+const mem = std.mem;
 
 const data = @embedFile("data/input_day01.txt");
 
 const word_digit = [_][]const u8{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
 fn search(is_part2: bool, input: []const u8) ?u8 {
-    return util.fmt.charToDigit(input[0], 10) catch blk: {
+    return fmt.charToDigit(input[0], 10) catch blk: {
         if (is_part2 == false) break :blk null;
         if (input.len < word_digit[0].len) break :blk null;
 
         for (word_digit, 1..) |term, i| {
             if (input.len < term.len) continue;
 
-            if (util.mem.eql(u8, input[0..term.len], term))
+            if (mem.eql(u8, input[0..term.len], term))
                 break :blk @intCast(i);
         }
         break :blk null;
@@ -20,7 +23,7 @@ fn search(is_part2: bool, input: []const u8) ?u8 {
 }
 
 fn solve(is_part2: bool, read_buff: []const u8) u32 {
-    var it = util.mem.split(u8, read_buff, "\n");
+    var it = mem.split(u8, read_buff, "\n");
 
     var total_sum: u32 = 0;
     while (it.next()) |line| {
@@ -45,6 +48,6 @@ fn solve(is_part2: bool, read_buff: []const u8) u32 {
 pub fn main() !void {
     util.printday(1);
 
-    util.std.debug.print("[Part 1] result total : {d}\n", .{solve(false, data)});
-    util.std.debug.print("[Part 2] result total : {d}\n", .{solve(true, data)});
+    std.debug.print("[Part 1] result total : {d}\n", .{solve(false, data)});
+    std.debug.print("[Part 2] result total : {d}\n", .{solve(true, data)});
 }
